5e39f6c68d8ffecf73c0a14f4d1653a18736abd4
Archit Kumar <architkumar@QAIT.COM>
2020-01-15 19:52:20 +0530

    Added substraction

--- src/com/qait/calculator/calculatorFunction.java
+++ src/com/qait/calculator/calculatorFunction.java
@@ -5,9 +5,16 @@ public class calculatorFunction {
 	public static int add(int a, int b) {
 		return a+b;
 	}
+	
+	
+	public static int substraction(int a, int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(add(2, 3));
+		
+		System.out.println(substraction(5, 2));
 	}
 
 }
